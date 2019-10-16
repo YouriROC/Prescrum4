90c3f80b5d9ab7185c5a7ea4267f074d2f3f5b08
Youri <0320494@student.rocvantwente.nl>
2019-10-16 08:45:59 +0200

    new cards etc

--- cardset.py
+++ cardset.py
@@ -1,6 +1,9 @@
 import pygame as pg
 import pygame.freetype
 import random
+
+from time import sleep
+
  
  
 def random_color():
@@ -11,65 +14,68 @@ def random_color():
     return (r, g, b)
 
 
-# initialise pygame
+# init pygame
 pg.init()
 clock = pg.time.Clock()
  
+#############################################################
+#Vanaf hier Tekst / punten / clock
+    
+#variablen
+p1punten = 0
+p2punten = 0
+timer = 0
+yellow = (255, 211, 0)
+zwart = (0,0,0) 
+beurtvanplayers = ("?")
+
+# text settings
+X = 300
+Y = 150
+
+font = pg.font.Font('fonts/joystix.ttf', 20) 
+  
+# set text to screen
+player1 = font.render("Player1's points:" + str(p1punten) , True, yellow)
+player2 = font.render("Player2's points:" + str(p2punten) , True, yellow)
+puntentext = font.render("Punten van players" , True, zwart)
+clockFont = font.render("Timer :" + str(timer), True, zwart)
+beurt = font.render("beurt van :" , True, zwart)
+beurtvanplayer = font.render(beurtvanplayers, True, yellow)
+# set text coords
+puntenrect = puntentext.get_rect()  
+puntenrect.center = (X // 1.8,(Y - 100) // 2)
+
+p1rect = player1.get_rect()  
+p1rect.center = (X // 1.8, Y // 2) 
+
+p2rect = player2.get_rect()  
+p2rect.center = (X // 1.8, (Y + 80) // 2) 
+
+clockrect = clockFont.get_rect()  
+clockrect.center = (X // 4,(Y + 1290) // 2) 
+
+beurtrect = beurt.get_rect()  
+beurtrect.center = (X // 2,(Y + 500) // 2)
+
+beurtvanplayerrect = beurtvanplayer.get_rect()  
+beurtvanplayerrect.center = (X // 2,(Y + 550) // 2)
+
+
 SCREEN_WIDTH = 1450
 SCREEN_HEIGHT = 950
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+cards_flipped = []
  
-# load and store the card images
-filenames = [
-        'images/cards/cardA.png',
-        'images/cards/cardB.png',
-        'images/cards/cardC.png',
-        'images/cards/cardD.png',
-        'images/cards/cardE.png',
-        'images/cards/cardF.png',
-        'images/cards/cardG.png',
-        'images/cards/cardH.png',
-        'images/cards/cardI.png',
-        'images/cards/cardJ.png',
-        'images/cards/cardA.png',
-        'images/cards/cardB.png',
-        'images/cards/cardC.png',
-        'images/cards/cardD.png',
-        'images/cards/cardE.png',
-        'images/cards/cardF.png',
-        'images/cards/cardG.png',
-        'images/cards/cardH.png',
-        'images/cards/cardI.png',
-        'images/cards/cardJ.png'
-        # etc.
-        # place all your card image filenames in this list
-        ]
-try:
-    # try loading the images from files
-    card_images = [pg.image.load(f).convert_alpha() for f in filenames]
-except Exception as e:
-    print(e)
-    # if loading fails, construct some dummy images
-    card_images = []
-    for i in range(20):
-        s = pg.Surface((48, 64))
-        s.fill(random_color())
-        card_images.append(s)
- 
- 
-# image of the card back
-cardback = pg.image.load('images/cards/card_back.png')
- 
-cards_flipped = 0
- 
- 
-# make a card class for displaying and possible interaction etc
+# make a card class
 class Card():
-    def __init__(self, image):
+    def __init__(self, image, letter):
         self.frontside = image.copy()
         self.image = self.frontside
         self.rect = image.get_rect()
         self.flipped = False
+        self.letter = letter
        
        
     def update(self, mouse_clicked):
@@ -77,42 +83,74 @@ class Card():
         if mouse_clicked:
             mpos = pg.mouse.get_pos()
             if self.rect.collidepoint(mpos):
-                self.flipped = True
-                global cards_flipped
-                cards_flipped += 1
+                if self.flipped == False:
+                    self.flipped = True
+                    global cards_flipped
+                    cards_flipped.append(self)
  
         # set the card's image depending on if it is flipped or not
         if not self.flipped:
             self.image = cardback.copy()
         else:
             self.image = self.frontside
-        if cards_flipped >= 2:
-            self.flipped = False
-            cards_flipped = 0
    
        
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
- 
+
+# load and store the card images
+possible_cards = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'
+]
 # create a bunch of card objects from the image
-cards = [Card(img) for img in card_images]
+cards = []
+amount_of_cards = len(possible_cards) * 2
+
+u = 0
+for i in range(0, amount_of_cards):
+    if(u == amount_of_cards / 2):
+        u = 0
+    letter_to_load = possible_cards[u]
+    
+    image = None
+    try:
+        image = pg.image.load("images/cards/card" + letter_to_load + ".png").convert_alpha()
+    except Exception as e:
+        image = pg.Surface((48, 64))
+        image.fill(random_color())
+    
+    cards.append(Card(image, letter_to_load))
+    u += 1
+
+ 
+# image of the card back
+cardback = pg.image.load('images/cards/card_back.png')
+ 
 # shuffle the card list in place
 random.shuffle(cards)
    
 # set each card's position on the screen
-horizontal_spacing =150
+horizontal_spacing = 150
 vertical_spacing = 200
 horizontal_indent = 750
 vertical_indent = 90
 marvertical_indent = 100
+
+def unflip_all_cards():
+    sleep(1.30)
+    global cards_flipped
+
+    for c in cards_flipped:
+        c.flipped = False
+
+    cards_flipped = []
  
 for i, card in enumerate(cards):
     pos_x = horizontal_indent + i % 5 * horizontal_spacing
     pos_y = vertical_indent + i // 5 * vertical_spacing
     card.rect.center = (pos_x, pos_y)
- 
- 
+
 running = True
 while running:
     mouse_pressed = False
@@ -127,76 +165,26 @@ while running:
    
     clock.tick(30)
    
-    bg_img = pg.image.load("images/background.png")  # loads image 
+    bg_img = pg.image.load("images/background.png")  # loads image
+
     screen.blit(bg_img, (1, 1)) 
+    screen.blit(player1, p1rect)
+    screen.blit(player2, p2rect)
+    screen.blit(puntentext, puntenrect)
+    screen.blit(clockFont, clockrect)
+    screen.blit(beurt, beurtrect)
+    screen.blit(beurtvanplayer,beurtvanplayerrect)
+
    
-    # draw the cards
+    # Unflip
+    if(len(cards_flipped) >= 2):
+        unflip_all_cards()
+
     for c in cards:
         c.update(mouse_pressed)
         c.draw(screen)
    
     pg.display.flip()
-   
-#############################################################
-#Vanaf hier Tekst / punten / clock
-    
-#variablen
-p1punten = 0
-p2punten = 0
-timer = 0
-yellow = (255, 211, 0)
-zwart = (0,0,0) 
-beurtvanplayers = ("?")
-
-#beurt player
-
-# text from here on
-X = 300;
-Y = 150;
-
-font = pg.font.Font('fonts/joystix.ttf', 20) 
-  
-# create a text suface object, 
-# on which text is drawn on it. 
-player1 = font.render("Player1's points:" + str(p1punten) , True, yellow)
-player2 = font.render("Player2's points:" + str(p2punten) , True, yellow)
-puntentext = font.render("Punten van players" , True, zwart)
-clock = font.render("Timer :" + str(timer), True, zwart)
-beurt = font.render("beurt van :" , True, zwart)
-beurtvanplayer = font.render(beurtvanplayers, True, yellow)
-# set the center of the rectangular object. 
-puntenrect = puntentext.get_rect()  
-puntenrect.center = (X // 1.8,(Y - 100) // 2)
-
-p1rect = player1.get_rect()  
-p1rect.center = (X // 1.8, Y // 2) 
-
-p2rect = player2.get_rect()  
-p2rect.center = (X // 1.8, (Y + 80) // 2) 
-
-clockrect = clock.get_rect()  
-clockrect.center = (X // 4,(Y + 1290) // 2) 
-
-beurtrect = beurt.get_rect()  
-beurtrect.center = (X // 2,(Y + 500) // 2)
-
-beurtvanplayerrect = beurtvanplayer.get_rect()  
-beurtvanplayerrect.center = (X // 2,(Y + 550) // 2)
-
-screen.blit(player1, p1rect)
-screen.blit(player2, p2rect)
-screen.blit(puntentext, puntenrect)
-screen.blit(clock, clockrect)
-screen.blit(beurt, beurtrect)
-screen.blit(beurtvanplayer,beurtvanplayerrect)
-
-pg.display.flip()
-  
-running = True
-while running:
-  for event in pg.event.get():
-    if event.type == pg.QUIT:
-      running = False
 
 pg.quit()
 
